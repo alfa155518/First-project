d24d4bd1e2a28795da0fcd3a2e9fa4a04ce28dde
Ahmed Hassop <alfaelkanas@gmail.com>
2024-02-26 01:07:23 +0200

    finish project

--- js/business-management.js
+++ js/business-management.js
@@ -1,3 +1,23 @@
+// Handle menu Links
+
+const bars = document.querySelector(".bars");
+const linksContainer = document.querySelector("nav .links");
+const allLinks = document.querySelectorAll("nav .links li");
+
+// bars to remove class appear
+bars.addEventListener("click", () => {
+  linksContainer.classList.toggle("active-links");
+});
+
+// Links to remove class appear
+allLinks.forEach((li) => {
+  li.addEventListener("click", () => {
+    if (linksContainer.classList.contains("active-links")) {
+      linksContainer.classList.remove("active-links");
+    }
+  });
+});
+
 // Make Fade out To Book in business-management page
 
 const allBook = document.querySelectorAll(".wrapper-books .books .book");
